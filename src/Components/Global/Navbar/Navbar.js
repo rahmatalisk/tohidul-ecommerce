99ed4de471a020a4a35374b2dcf31c75e9c2442c
rahmatalisk <sk.rahmatali6792@gmail.com>
2023-10-14 21:08:03 +0600

    shape added

--- src/Components/Global/Navbar/Navbar.js
+++ src/Components/Global/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./Navbar.scss";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const navItems = [
@@ -24,23 +25,29 @@ const Header = () => {
       href: "/",
       active: false,
     },
-    
   ];
   return (
     <header>
+      <div className="shape"></div>
       <div id="navbar">
-        <div className="shape"></div>
+        
         <div className="container">
           <div className="nav">
             <div className="logo">
               <h2>Logo</h2>
             </div>
             <div className="nav-item">
-              {navItems.map((it, i) => (
-                <a className={`${it?.active && "active"}`} key={i} href="/">
-                 {it?.name}
-                </a>
-              ))}
+              <div className="nav-links">
+                {navItems.map((item, i) => (
+                  <Link
+                    className={item.active ? "active" : ""}
+                    key={i}
+                    to={item.href}
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
 
               <button className="login-btn">Login</button>
             </div>
